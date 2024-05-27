ff43f9f637d969c86c2217481b5557f16d5d2509
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-05-27 12:04:02 +0900

    [FEAT] 아니 왜 이러는거야

--- Frontend/newsgenerator/src/App.js
+++ Frontend/newsgenerator/src/App.js
@@ -3,11 +3,6 @@ import styled from 'styled-components'
 import NavBarComponent from './components/NavBar';
 import logo from './images/logo.png';
 import FormOpBtn from './components/FormOpBtn';
-<<<<<<< HEAD
-//import logo from './images/logo.png';
-=======
->>>>>>> feature-formopbtn
-
 function App() {
   return (
     <Frame>
@@ -20,12 +15,7 @@ function App() {
       <TextBox>
         <TextForm />
       </TextBox>
-      <BtnBox>
-        <FormOpBtn />
-      </BtnBox>
-      
       </MainBox>
-      <FormOpBtn />
     </Frame>
 
   );
@@ -43,7 +33,7 @@ align-items: stretch;
 const MainBox = styled.div`
   width: 100%;
   display: grid;
-  grid-template-rows: 0.5fr 1.5fr 3fr 3fr; // 4개의 행으로 구성, 각 행의 비율 조정
+  grid-template-rows: 0.5fr 1.5fr 5fr 3fr; // 4개의 행으로 구성, 각 행의 비율 조정
   align-items: stretch; // 수직 방향으로 가운데 정렬
 `
 
@@ -59,20 +49,19 @@ display:flex;
 flex-direction: column;
 align-items:center;
 justify-content:center;
+
 box-sizing: border-box; 
 `
 const TextForm = styled.textarea`
-  width: 70%;
+  width: 60%;
   height: 90%;
   padding: 2vw;
   border-radius: 10px;
   border-style: none;
   box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.25);
-  box-sizing: border-box; 
+
 `
-
-
-const GuideBanner = styled.a`
+const GuideBanner = styled.div`
   width: 100%;
   background-color: transparent;  // 배경색 변경
   align-items: right;
@@ -81,20 +70,9 @@ const GuideBanner = styled.a`
   font-weight: bold;
   text-align: right;
   font-size: 1vw;
-  color: #0089CF; 
-  cursor: pointer;
+  color: #0089CF;
+  
 `;
-
-const BtnBox = styled.div`
-width: 75%;
-height: 90%;
-display: flex;
-justify-content: right; 
-padding: 3vw;
-box-sizing: border-box; 
-margin: 0 auto; // 자동 마진을 사용하여 좌우 중앙 정렬
-`
-
 
 
 export default App;
