e9518b53ea9ebb79a7910af9df7c380c977a820b
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-05-27 10:26:31 +0900

    [FEAT] mainpage d위쪽배너 및 텍스트입력 부분 구현 #12

--- Frontend/newsgenerator/src/App.js
+++ Frontend/newsgenerator/src/App.js
@@ -9,9 +9,12 @@ function App() {
     <Frame>
       <NavBarComponent />
       <MainBox>
-      <TitleBox src={logo}/>
+        <GuideBanner>사용자 가이드</GuideBanner>
+      <TitleBox>
+        <img src={logo} alt="Main Logo"style={{width: "15%"}}/>
+      </TitleBox>
       <TextBox>
-        안녕
+        <TextForm />
       </TextBox>
       </MainBox>
     </Frame>
@@ -19,41 +22,58 @@ function App() {
   );
 }
 
-
 const Frame = styled.div`
-  width:95vw;
-  height: 50vw; 
-  background-color:#F5F6FA;
-  display: flex;
+width:95vw;
+height: 50vw; 
+background-color:#F5F6FA;
+display: flex;
+flex-directon: column;
+align-items: stretch;
 `
 
 const MainBox = styled.div`
-width: 100%;
-display:flex;
-flex-direction:column;
-justify-content:center;
-align-items:center;
+  width: 100%;
+  display: grid;
+  grid-template-rows: 0.5fr 1.5fr 5fr 3fr; // 4개의 행으로 구성, 각 행의 비율 조정
+  align-items: stretch; // 수직 방향으로 가운데 정렬
 `
 
-const TitleBox = styled.img`
-  width: 20%;
-  margin: 5%;
-  
+const TitleBox = styled.div`
+  width: 100%;
+  display: flex;
+  align-items:center;
+  justify-content:center;
 `
 
-const TextBox = styled.textarea`
+const TextBox = styled.div`
 display:flex;
 flex-direction: column;
-border-radius: 10px;
-border-style: none;
-box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.25);
-width: 45vw;
-height: 25vw;
-padding: 2vw;
+align-items:center;
+justify-content:center;
+
 box-sizing: border-box; 
 `
+const TextForm = styled.textarea`
+  width: 60%;
+  height: 90%;
+  padding: 2vw;
+  border-radius: 10px;
+  border-style: none;
+  box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.25);
 
-
+`
+const GuideBanner = styled.div`
+  width: 100%;
+  background-color: transparent;  // 배경색 변경
+  align-items: right;
+  padding: 20px;
+  box-sizing: border-box; 
+  font-weight: bold;
+  text-align: right;
+  font-size: 1vw;
+  color: #0089CF;
+  
+`;
 
 
 export default App;
