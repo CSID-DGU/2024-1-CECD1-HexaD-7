121e3643fc63f5641785fcc3d1657433799e5567
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-05-29 16:45:38 +0900

    [FEAT] userlogin API 연동 완료

---
+++ Frontend/newsgenerator/src/api/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const API = axios.create({
+  baseURL: "http://127.0.0.1:8000/",
+
+  headers: {
+    "Content-Type": "application/json"
+  },
+  //withCredentials: true
+});
+
+export default API;
--- Frontend/newsgenerator/src/pages/Main.js
+++ Frontend/newsgenerator/src/pages/Main.js
@@ -27,6 +27,8 @@ function Main() {
   }
 
 
+
+
   return (
     <Frame>
       <NavBarComponent />
