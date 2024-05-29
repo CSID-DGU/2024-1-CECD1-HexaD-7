bf5bbdb1b5592aac0029a8ec1466023cb893892b
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-05-29 16:25:29 +0900

    [FEAT] CORS 전체 출처 허용으로 설정 변경

--- backend/newsgenerator/newsgenerator/settings.py
+++ backend/newsgenerator/newsgenerator/settings.py
@@ -38,10 +38,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "userlogin"
+    "userlogin",
+    "corsheaders",
+
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -50,6 +54,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "newsgenerator.urls"
 
--- backend/newsgenerator/userlogin/views.py
+++ backend/newsgenerator/userlogin/views.py
@@ -7,7 +7,7 @@ from rest_framework import status
 class VerifyAccessCode(APIView):
     def post(self, request):
         access_code = request.data.get('access_code')
-        if access_code == "valid_code": #임의코드
+        if access_code == "240520": #임의코드
             return Response({'status': 'success', 'message': 'Access code verified successfully.'}, status=status.HTTP_200_OK)
         else:
             return Response({'status': 'failure', 'error_code': 401, 'message': 'Invalid access code. Please check and try again.'}, status=status.HTTP_401_UNAUTHORIZED)
