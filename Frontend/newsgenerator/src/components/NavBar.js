21ddd36946b3837068cac1c610d8dcf7b3cbf292
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-06-17 05:00:10 +0900

    [FEAT] NavBar 컴포넌트 navigate 적용

--- Frontend/newsgenerator/src/components/NavBar.js
+++ Frontend/newsgenerator/src/components/NavBar.js
@@ -1,82 +1,106 @@
-import React from 'react'
-import styled from 'styled-components'
-import pencil from '../images/pencil.png'
-import document from '../images/document.png'
-import corpus from '../images/corpus.png'
-import logo from '../images/logo.png'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import pencil from '../images/pencil.png';
+import document from '../images/document.png';
+import corpus from '../images/corpus.png';
+import logo from '../images/logo.png';
+import { useNavigate } from 'react-router-dom';
+
 const NavBarComponent = () => {
-    return (
-        <NavBar>
-        <LogoImg src={logo} />
-        <div style={{textAlign: 'left', width: '100%'}}>
-        <p style={{fontSize: '1vw'}}>Menu</p>
-        </div>
-        <BtnComponent>
-        <NavBarBtn text="기사 생성" icon={pencil}/>
-        <NavBarBtn text="기사작성 피드백" icon={document}/>
-        <NavBarBtn text="주제 제안 기능" icon={corpus}/>
-        </BtnComponent>
-      </NavBar>
+  const [activeBtn, setActiveBtn] = useState('');
+  const navigate = useNavigate();
 
-    )
-
-}
-export default NavBarComponent;
-
-const NavBarBtn = ({text, icon}) => {
-    return (
-      <NavBarBtnStyle>
-        {icon && <img src={icon} alt={text} style={{ marginRight: '10px', width: '18%'}} />}
-        {text}
-      </NavBarBtnStyle>
-    )
+  const handleNavigate = (path, btnKey) => {
+    setActiveBtn(btnKey);
+    navigate(path);
   }
-  
-  const BtnComponent = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-  `
-  
-  
-  const LogoImg = styled.img`
-    margin: 20px;
-    width: 70%;
-  `
 
-  
-const NavBar = styled.div`
-padding: 10px 30px;
-align-items: center;
-display:flex;
-flex-direction: column;
-background-color: #FFFFFF;
-width: 18vw;
-height: 60vw;
-box-sizing: border-box; 
-grid-auto-rows: minmax(1fr, 3fr, 2fr);
+  return (
+    <NavBar>
+      <LogoImg src={logo} />
+      <div style={{textAlign: 'left', width: '100%'}}>
+        <p style={{fontSize: '1vw'}}>Menu</p>
+      </div>
+      <BtnComponent>
+        <NavBarBtn 
+          text="기사 생성" 
+          icon={pencil} 
+          isActive={activeBtn === 'generatearticle'}
+          onClick={() => handleNavigate('/main', 'generatearticle')}
+        />
+        <NavBarBtn 
+          text="기사작성 피드백" 
+          icon={document} 
+          isActive={activeBtn === 'feedback'}
+          onClick={() => handleNavigate('/feedback', 'feedback')}
+        />
+        <NavBarBtn 
+          text="주제 제안 기능" 
+          icon={corpus} 
+          isActive={activeBtn === 'topicgenerator'}
+          onClick={() => handleNavigate('/topicgenerator1', 'topicgenerator')}
+        />
+      </BtnComponent>
+    </NavBar>
+  );
+}
 
-`
+
+const NavBarBtn = ({ text, icon, onClick, isActive }) => {
+  return (
+    <NavBarBtnStyle onClick={onClick} isActive={isActive}>
+      {icon && <img src={icon} alt={text} style={{ marginRight: '10px', width: '18%' }} />}
+      {text}
+    </NavBarBtnStyle>
+  );
+}
 
 const NavBarBtnStyle = styled.button`
-margin: 1vw 0vw;
-color: black;
-text-align:left;
-font-weight: bold;
-font-size: 1vw;
-display:flex;
-align-items: center;
-width: 15vw;
-height: 2.3vw;
-padding: 1.7vw;
-border-radius: 0.5vw;
-border-style: none;
-background-color: #FFFFFF;
-&:hover{
-  color:white;
-  background-color: #0089CF;
-}
+  margin: 1vw 0vw;
+  color: ${props => props.isActive ? 'white' : 'black'};
+  background-color: ${props => props.isActive ? '#0089CF' : '#FFFFFF'};
+  text-align: left;
+  font-weight: bold;
+  font-size: 1vw;
+  display: flex;
+  align-items: center;
+  width: 15vw;
+  height: 2.3vw;
+  padding: 1.7vw;
+  border-radius: 0.5vw;
+  border-style: none;
+  &:hover {
+    background-color: #0089CF;
+    color: white;
+  }
+    &:click {
+    background-color: #0089CF;
+    color: white;
+  }
+`;
 
-`
-  
+const BtnComponent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const LogoImg = styled.img`
+  margin: 20px;
+  width: 70%;
+`;
+
+const NavBar = styled.div`
+  padding: 10px 30px;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  background-color: #FFFFFF;
+  width: 18vw;
+  height: 60vw;
+  box-sizing: border-box;
+`;
+
+
+export default NavBarComponent;
