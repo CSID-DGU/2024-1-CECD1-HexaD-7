050254ff101a29de482efe531326895f238ad118
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-06-19 21:54:39 +0900

    *.pyc 파일 삭제

--- backend/newsgenerator/feedback/models.py
+++ backend/newsgenerator/feedback/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Article(models.Model):
+    
     title = models.CharField(max_length=255)
     draft = models.TextField()
     article_type = models.CharField(max_length=100)
